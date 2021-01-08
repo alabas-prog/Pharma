0e7b512c9a5434636775527081872fd9b8a935cf
Ahmed <ahmad.abdallah.mohamad@gmail.com>
2021-01-08 22:31:51 +0200

    invoice  page

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let db = openDatabase('pharmaDB', '1.0', 'Pharma DataBase', 50 * 1024 * 1024);
 
 db.transaction(function (tx) {
+    //tx.executeSql('DROP TABLE Invoice ');
     tx.executeSql('CREATE TABLE IF NOT EXISTS Users (username unique, password)');
-    tx.executeSql('insert into Users values ("Admin","123")');
     tx.executeSql('CREATE TABLE IF NOT EXISTS Items (name unique, quantity, picture)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Invoice (date, customerName, type, items, quantity)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Invoice (invoiceNum, date, customerName, type, items, quantity)');
+    // tx.executeSql('insert into Users values ("Admin","123")');
 });
 
 function validate() {
@@ -156,3 +157,82 @@ db.transaction(function (tx) {
         null
     );
 });
+
+// *********************Invoice**************************
+date = new Date().toDateString() + ' ' + new Date().toLocaleTimeString();
+console.log(date)
+
+function addInvoiceItem() {
+    let type = $('#type').val();
+    let customerName = $('#customerName').val();
+    let itemName = $('#itemName').val();
+    let qty = $('#qty').val();
+
+    $('#invoiceTable').append("<tr><td>" + itemName + "</td><td>" + qty + "</td></tr>")
+
+    $('#itemName').val('');
+    $('#qty').val('');
+}
+
+function saveInvoice() {
+
+    db.transaction(function (tx) {
+        tx.executeSql('select max(invoiceNum)a from Invoice',[],
+        function (tx, results) {
+            console.log("*********")
+            
+            var invoiceNum = 1;
+            var row = results.rows[0]//Error in object
+
+            console.log(row['a'])
+            console.log("#########")
+
+            if(results.rows.item(0)['a'] == null){
+                invoiceNum = 1;
+            }else{
+                invoiceNum = parseInt(results.rows[0][0])+1;
+            }
+
+            console.log("invoiceNum")
+            console.log(invoiceNum)
+            
+
+            $('#invoiceTable tr').each(function () {
+                // date, customerName, type, items, quantity
+                let sql = 'insert into Invoice (invoiceNum, date,customerName, type,items,quantity) values ("' +
+                invoiceNum + '","' +
+                date + '","' +
+                    $('#customerName').val() + '","' +
+                    $('#type').val() + '"'
+                    $(this).find('td').each(function () {
+                        sql += ',"' + $(this).html() + '"'
+                    });
+                    sql += ')\n'
+                    console.log(sql)
+                    db.transaction(function (tx) {
+                    tx.executeSql(sql);
+                });
+            });
+        },function(){
+            console.log("error")
+            var invoiceNum = 1;            
+            $('#invoiceTable tr').each(function () {
+                let sql = 'insert into Invoice (invoiceNum, date,customerName, type,items,quantity) values ("' +
+                invoiceNum + '","' +
+                date + '","' +
+                    $('#customerName').val() + '","' +
+                    $('#type').val() + '"'
+                    $(this).find('td').each(function () {
+                        sql += ',"' + $(this).html() + '"'
+                    });
+                    sql += ')\n'
+                    console.log(sql)
+                    db.transaction(function (tx) {
+                    tx.executeSql(sql);
+                });
+            });
+        }) 
+    });
+
+        
+}
